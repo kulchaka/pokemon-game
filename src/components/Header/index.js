41df4fd9b226408a76b325f8fb71e2fca4ba1177
Andrii <kulchaka@gmail.com>
2021-03-10 21:24:52 +0100

    refactoring Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import h from './style.module.css'
 
-const Header = (props) => {
+const Header = ({title, descr}) => {
     return (
         <header className={h.root}>
             <div className={h.forest}></div>
             <div className={h.container}>
-                <h1>{props.title}</h1>
-                <p>{props.descr}</p>
+                <h1>{title}</h1>
+                <p>{descr}</p>
             </div>
         </header>
     )
