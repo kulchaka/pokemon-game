b0de9acc98e6bd86b9e88f58249a9ef7a9269ea7
Andrii <kulchaka@gmail.com>
2021-03-18 07:46:26 +0100

    finish HW4

--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -1,4 +1,6 @@
 import MenuHeader from "../../components/MenuHeader";
+import s from './style.module.css'
+import Footer from "../../components/Footer";
 
 const GamePage = ({onChangePage}) => {
 
@@ -9,9 +11,12 @@ const GamePage = ({onChangePage}) => {
 
     return (
         <>
-            <MenuHeader bgActive={true} />
+            <MenuHeader bgActive />
+            <div className={s.container}>
             <h1>This is our GAME PAGE!</h1>
             <button onClick={handler}>BACK</button>
+            </div>
+            <Footer />
 
         </>
     )
